e425578b0444e084654b966595ccf182d03b5d14
kevin_HWS <56685204+kevinshu1995@users.noreply.github.com>
2021-04-15 20:51:39 +0800

    Button style
    1. a 增加 href
    2. 按鈕文字更換為svg

--- SideBarToggleButtonOnTeachable.user.js
+++ SideBarToggleButtonOnTeachable.user.js
@@ -4,7 +4,7 @@
 // @namespace    https://kevinshu1995.github.io/blog/
 // @author       Kevin Hsu
 // @license      MIT
-// @version      0.1
+// @version      0.2
 // @include      *://courses.hexschool.com/*
 // @grant        none
 // @homepage     https://kevinshu1995.github.io/blog/
@@ -12,6 +12,7 @@
 // @source       https://gist.github.com/kevinshu1995/9691a0206bc9222b73334b4f3c17b759
 // ==/UserScript==
 (function (){
+    const svg = `<svg x="0px" y="0px" viewBox="0 0 40 34" enable-background="new 0 0 40 34" focusable="false" style="fill: rgb(255, 255, 255);height: 100%;left: 0px;stroke-width: 0px;top: 0px;width: 3rem;/* height: 1rem; *//* width: 1rem; */"><g><g><polyline fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="31.4,12.6 31.4,8.7 25.8,8.7"></polyline><polyline fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="14.7,8.7 9.1,8.7 9.1,12.6"></polyline><polyline fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="25.8,24.8 31.4,24.8 31.4,20.9"></polyline><polyline fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="9.1,20.9 9.1,24.8 14.7,24.8"></polyline></g><rect x="13.7" y="12.3" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" enable-background="new" width="13.3" height="8.9"></rect></g></svg>`
     const toggle = new Toggle()
     const keyMap = {}
     const keyMapHandler = function(e){
@@ -57,10 +58,12 @@
     }){
         const fragment = document.createDocumentFragment()
         const btn = document.createElement('a')
+        btn.style.padding = '0.1rem'
         btn.classList.add(o.classList)
         btn.id = o.id
-        btn.textContent = o.btnText
+        btn.innerHTML = svg
         btn.setAttribute('role', 'button')
+        btn.setAttribute('href', '#')
         listenToggleBtn(btn)
         fragment.appendChild(btn)
         document.querySelector('.lecture-nav').appendChild(fragment)
